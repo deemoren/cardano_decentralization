9d9d5e2d99045afd62a15251ab80c4069add6051
Deemo <62387733+deemoren@users.noreply.github.com>
2021-08-12 00:56:37 +0100

    Update most_recent_api.py

--- api/api_collection/most_recent_api.py
+++ api/api_collection/most_recent_api.py
@@ -3,15 +3,8 @@ from api.base.dto.api_output import ApiOutput
 import psycopg2
 
 class Api(BaseApi):
-    """
-    这个 api 的访问路径为：/test/hello
-    无需定义路由，文件路径即 api 路径。
-    """
 
     def run(self, input) -> ApiOutput:
-        """
-        这里的 input 为 api 入参，类型为 dict。
-        """
                 
         DB_NAME = "cexplorer"
         DB_HOST = "/var/run/postgresql"
@@ -57,9 +50,5 @@ class Api(BaseApi):
         cur.close()
         conn.close()
 
-
-        # 访问数据库的操作封装起来，一处定义，多处使用
-        # db.query_as_pd("SELECT * from table")
-        # db.query("SELECT * from table")
 
         return ApiOutput.success(str(x))
