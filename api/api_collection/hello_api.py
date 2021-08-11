494bbe55befa9ce91908fee839808e6dc286cc12
Deemo <62387733+deemoren@users.noreply.github.com>
2021-08-12 00:54:35 +0100

    Update hello_api.py

--- api/api_collection/hello_api.py
+++ api/api_collection/hello_api.py
@@ -2,18 +2,7 @@ from api.base.base_api import BaseApi
 from api.base.dto.api_output import ApiOutput
 
 class Api(BaseApi):
-    """
-    这个 api 的访问路径为：/test/hello
-    无需定义路由，文件路径即 api 路径。
-    """
 
     def run(self, input) -> ApiOutput:
-        """
-        这里的 input 为 api 入参，类型为 dict。
-        """
-
-        # 访问数据库的操作封装起来，一处定义，多处使用
-        # db.query_as_pd("SELECT * from table")
-        # db.query("SELECT * from table")
 
         return ApiOutput.success("hello, you have tested successfully")
