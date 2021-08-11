d422904fd47cf082f3b40e5c1eef3ff0fb96d99c
Deemo <62387733+deemoren@users.noreply.github.com>
2021-08-12 00:58:05 +0100

    Update pool_info_api.py

--- api/api_collection/pool_info_api.py
+++ api/api_collection/pool_info_api.py
@@ -6,10 +6,7 @@ import json
 
 
 class Api(BaseApi):
-    """
-    这个 api 的访问路径为：/test/hello
-    无需定义路由，文件路径即 api 路径。
-    """
+
 
     def run(self, input) -> ApiOutput:
                 
@@ -21,10 +18,7 @@ class Api(BaseApi):
 	DB_USER = "siri"
 	DB_PASS = "PasswordYouWant"
 	DB_PORT =  "5432"
-	"""
-	这里的 input 为 api 入参，类型为 dict。
-	"""
-	# 连接数据库
+
 	conn = psycopg2.connect(
 			            dbname   =   DB_NAME,
 			            host     =   DB_HOST,
@@ -42,7 +36,6 @@ class Api(BaseApi):
 	    return x_df
 
 
-	#  pool owner的信息
 	sql_pool_owner_addr = """
 	select epoch_no, pool_hash_id, addr_id from v_pool_owners_by_epoch;
 	"""
@@ -50,7 +43,7 @@ class Api(BaseApi):
 	pool_owner_addr.columns = [ "epoch_no", "pool_hash_id",  "owner_address_id"]
 	pool_owner_addr["is_owner"] = 1
 
-	# delegators 的信息
+
 	sql_delegators_info = ''' select delegation.active_epoch_no,stake_address.id, pool_hash.id,epoch_stake.amount
 	from delegation 
 	inner join stake_address on delegation.addr_id = stake_address.id
@@ -62,18 +55,16 @@ class Api(BaseApi):
 	delegators_info['is_delegator'] =1
 
 
-	# 所有人的 stake 地址
 	sql_ddr = """
 	select id, view from stake_address;
 	"""
 	addr_relay = from_sql_to_df(sql_ddr)
 	addr_relay.columns = [ "addr_id", "stake_address"]
 
-	# epoch+pool 去对应owner和delegator
+
 	owner_delegator = pool_owner_addr.merge(delegators_info, on=['epoch_no','pool_hash_id'], how = 'outer')
 	owner_delegator_addr = owner_delegator.merge(addr_relay, on = ['addr_id'], how = 'left')
 
-	# 每个pool在不同epoch产生的block数
 	sql_block_pool = """
 	select block.epoch_no, pool_hash.id, count (*) as block_count from block 
 	inner join slot_leader on block.slot_leader_id = slot_leader.id
@@ -83,7 +74,7 @@ class Api(BaseApi):
 	block_pool = from_sql_to_df(sql_block_pool)
 	block_pool.columns = [ "epoch_no", "pool_hash_id", "block_amount"]
 
-	#每个pool 在不同 epoch的pledge / amount by epoch
+
 	sql_stake_distribution = "SELECT pool_id, sum (amount), epoch_no FROM epoch_stake GROUP BY pool_id, epoch_no ;"
 	stake_distribution = from_sql_to_df(sql_stake_distribution)
 	stake_distribution.columns = [ "pool_hash_id","pool_amount", "epoch_no"]
@@ -97,14 +88,14 @@ class Api(BaseApi):
 	block_pledge_amount = block_pledge.merge(stake_distribution, on=['epoch_no','pool_hash_id'], how = 'outer')
 
 
-	# 加上每个pool的地址信息
+
 	sql_pool_id_view = "select id, view from pool_hash;"
 	pool_id_view = from_sql_to_df(sql_pool_id_view)
 	pool_id_view.columns = ['pool_hash_id','pool_hash_view']
 	block_pledge_amount = block_pledge_amount.merge(pool_id_view, on = ['pool_hash_id'], how = 'left')
 
 	print(block_pledge_amount.head())
-	# 把所有信息merge在一起
+
 	pool_info = pd.merge(block_pledge_amount,owner_delegator_addr, on = ['epoch_no','pool_hash_id'], how = "outer" )
 
 	result = pool_pledge_0_owner.to_json(orient="records")
